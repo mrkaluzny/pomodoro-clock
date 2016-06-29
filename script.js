04527d2c612e8b2b9eaa87864bbe203e3266e46b
Wojciech Kaluzny <wojciech.kaluzny@me.com>
2016-06-29 11:46:40 +0200

    Basic functionality, copied from CodePen

---
+++ script.js
@@ -0,0 +1,39 @@
+var sessionLength = 25;
+var breakLength = 5;
+
+$(document).ready(function(){
+   $(".switchAddSession").on("click", function(){
+       sessionLength += 1;
+       $("#timer-session").text(sessionLength);
+       $("#timeLeft").text(sessionLength);
+   });
+
+    $(".switchDeduceSession").on("click", function(){
+       if (sessionLength > 1){
+            sessionLength -= 1;
+        } else {
+            sessionLength = 1;
+        }
+       $("#timer-session").text(sessionLength);
+        $("#timeLeft").text(sessionLength);
+   });
+
+    $(".switchAddBreak").on("click", function(){
+       breakLength += 1;
+       $("#timer-break").text(breakLength);
+   });
+
+    $(".switchDeduceBreak").on("click", function(){
+        if (breakLength > 1){
+            breakLength -= 1;
+        } else {
+            breakLength = 1;
+        }
+       $("#timer-break").text(breakLength);
+   });
+
+    $('.countdown-clock').on('click', function(){
+
+    });
+
+});
